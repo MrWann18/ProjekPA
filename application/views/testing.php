5e9ffce078ebbfef50ff4c7572db67e4d6562e47
MrWann18 <nanda20ti@mahasiswa.pcr.ac.id>
2024-03-01 10:26:30 +0700

    First Commit

---
+++ application/views/testing.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Proyek PA</title>
+</head>
+<body>
+    <H1>Nanda kur</h1>
+    <H2>Judul PA: APLIKASI VALIDASI TANDA TANGAN BERKAS PROYEK AKHIR MELALUI QR CODE MENGGUNAKAN METODE PROTOTYPE</H2>
+    <p>testing</p>
+</body>
+</html>
